35aab21bf45ea193f505dc39189965982713d5e8
Harikesh <harikeshreji@gmail.com>
2026-01-29 18:19:28 +0530

    basic chatbot implemented

--- src/langgraphagenticai/LLMS/groqllm.py
+++ src/langgraphagenticai/LLMS/groqllm.py
@@ -1,1 +1,20 @@
+import os
+import streamlit as st 
+from langchain_groq import ChatGroq
 
+class GroqLLM:
+    def __init__(self,user_controls_input):
+        self.user_controls_input = user_controls_input
+    
+    def get_llm_model(self):
+        try:
+            groq_api_key=self.user_controls_input["GROQ_API_KEY"]
+            model_name=self.user_controls_input["selected_model"]
+            if groq_api_key==' ' and os.environ["GROQ_API_KEY"]==' ':
+                st.error("Please enter the groq api key")
+
+            llm = ChatGroq(api_key=groq_api_key,model=model_name)
+        
+        except Exception as e:
+            raise ValueError(f"Error Occured with Exception: {e}")
+        return llm
--- src/langgraphagenticai/nodes/basic_chatbot_node.py
+++ src/langgraphagenticai/nodes/basic_chatbot_node.py
@@ -1,1 +1,11 @@
+from src.langgraphagenticai.state.state import State
 
+class BasicChatbotNode:
+    def __init__(self,model):
+        self.llm = model
+
+    def process(self,state:State)->dict:
+        """
+        Processes the input state and generates a chatbot response.
+        """
+        return {"messages":self.llm.invoke(state['messages'])}
